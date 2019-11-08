45e6006ce9b3ab158c867c756e24b9658b4505d2
Álvaro Zambrana Fernández <56718986+AlvaroZ-F@users.noreply.github.com>
2019-11-08 10:26:53 +0100

    update

---
+++ Unit 3/Exercise Table Students by course.php
@@ -0,0 +1,16 @@
+<!--
+	Write function ($idcurso) to find in the database all students signed up in that course
+	using pdo
+-->
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title></title>
+	</head>
+
+	<body>
+
+	</body>
+</html>
